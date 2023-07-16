5d2d24e56095554bff87b613ff5b446d6748b450
axi <huangwudong2011@gmail.com>
2023-07-17 00:55:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
 MD_HEAD = """
-#### [Github issues 博客](https://github.adone.eu.org/) & [Notion 博客](https://nb.adone.eu.org/) 喜欢您来
 [![](https://s2.loli.net/2023/07/03/WxmifsloVXrYz2I.png)](https://nb.adone.eu.org/)
+#### [Github issues 博客](https://github.adone.eu.org/) & [Notion 博客](https://nb.adone.eu.org/) 喜欢您来
 """
 BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 50
