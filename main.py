df70d612856014442b295a065f187fb4a9a4e51c
axi <huangwudong2011@gmail.com>
2023-07-17 00:12:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ MD_HEAD = """
 [![](https://s2.loli.net/2023/07/03/WxmifsloVXrYz2I.png)](https://nb.adone.eu.org/)
 """
 
-  </div>
-
-  <!-- Snake Code Contribution Map 贪吃蛇代码贡献图 -->
-<picture>
-  <source media="(prefers-color-scheme: dark)" srcset="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake-dark.svg" />
-  <source media="(prefers-color-scheme: light)" srcset="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake.svg" />
-  <img alt="github-snake" src="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake-dark.svg" />
-</picture>
-
-</div>
-
 BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 50
 TOP_ISSUES_LABELS = ["Top"]
